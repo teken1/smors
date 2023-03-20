19083e78f37a931451c3598be267aa5a1229169b
teken1 <tekevweemoghene@gmail.com>
2023-03-20 14:59:19 +0100

    login page finished

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,7 +2,8 @@ import { Login } from "@mui/icons-material";
 import React, { useMemo, useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 export const LoginPage = () => {
-  const [active,setActive] = useState("1");
+  const [passVisible,setPassVisibile] = useState("visibility");
+  const [passType,setPassType] = useState("password");
     const [navChecked, setNavChecked] = useState(false);
     const [fname, setFname] = useState("Daniel");
     const [capitalUSD, setCapitalUSD] = useState("0.00");
@@ -21,13 +22,25 @@ export const LoginPage = () => {
 
     }
 
+    const handleVisibility = () => {
+        if (passVisible == "visibility") {
+            setPassVisibile("visibility_off");
+            setPassType("text");
+            return;
+        } else {
+            setPassVisibile("visibility");
+            setPassType("password");
+            return;
+        }
+    }
+
 useEffect(() => {
     
 },[])
     return (
         <main style={{backgroundColor:"#1c1f42",
         }}>
-            <div style={{minHeight:"100vh", padding:"20px 50px" }}>
+            <div style={{minHeight:"100vh", padding:"20px 50px", position:"relative", overflowX:"hidden" }}>
                 <div className="logo-bg">
                 </div>
                 <div style={{textAlign:"left"}}>
@@ -44,7 +57,8 @@ useEffect(() => {
                         <input type="text" placeholder="Enter Username" />
                     </div>
                     <div className="form-control">
-                        <input type="password" placeholder="Enter Password" />
+                        <input type={passType} placeholder="Enter Password" />
+                        <div className="seepass"><span onClick={handleVisibility} className="material-icons grey">{passVisible}</span></div>
                     </div>
                     <p onClick={()=>window.location="/forgot-password"} className="forgotpass">I forgot my password</p>
                     <button className="gradient-btn" onClick={handleLogin}>Login Here</button>
